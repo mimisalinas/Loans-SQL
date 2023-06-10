a7a8b75a46abb1ee3f9e333437767254de59f201
mimisalinas <mimi.salinas@hotmail.com>
2023-06-10 14:01:16 +1000

    Categorical SUM

--- query4.sql
+++ query4.sql
@@ -1,1 +1,8 @@
--- Find which customer paid the most in loans
+-- Output which customer has the most in loan amounts
+SELECT SUM(loans.loan_amount) AS total_loans, customers.name
+FROM loans
+INNER JOIN customers ON customers.id = loans.customer_id
+GROUP BY customers.id 
+ORDER BY total_loans DESC
+LIMIT 1; 
+

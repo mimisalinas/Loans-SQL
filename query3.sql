0848095ad922cc735cb291a3dda23d2af355a20e
mimisalinas <mimi.salinas@hotmail.com>
2023-06-10 13:22:36 +1000

    Date specific query

--- query3.sql
+++ query3.sql
@@ -1,1 +1,6 @@
--- Output all John Doe's loans payments done before June
+-- Output all Jane Smith's loans payments done before 2023-06-01
+SELECT payments.amount, payments.date, loans.name
+FROM payments
+INNER JOIN loans ON loans.id = payments.loan_id
+INNER JOIN customers ON customers.id = loans.customer_id
+WHERE customers.name = 'Jane Smith' AND payments.date < '2023-06-01';

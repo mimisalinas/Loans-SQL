6fea8318e213d370d3dbf5c96625f6c86f139dcd
mimisalinas <mimi.salinas@hotmail.com>
2023-06-11 17:40:48 +1000

    Count payments

--- query6.sql
+++ query6.sql
@@ -1,1 +1,8 @@
 -- Output which customer made the most payments 
+SELECT COUNT(*) AS number_of_payments, customers.name
+FROM payments
+INNER JOIN loans ON loans.id = payments.loan_id
+INNER JOIN customers ON customers.id = loans.customer_id 
+GROUP BY customers.id
+ORDER BY number_of_payments DESC
+LIMIT 1;
